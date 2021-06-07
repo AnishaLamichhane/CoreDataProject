124e6d82813af38086c81454f25b3d532ded9e88
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-06-07 14:52:26 +0545

    optionality solutions of the core data

---
+++ project 12/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  project 12
+//
+//  Created by Anisha Lamichhane on 6/7/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
---
+++ project 12/Movie+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  Movie+CoreDataProperties.swift
+//  project 12
+//
+//  Created by Anisha Lamichhane on 6/7/21.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Movie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Movie> {
+        return NSFetchRequest<Movie>(entityName: "Movie")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var director: String?
+    @NSManaged public var year: Int16
+    
+    public var wrappedTitle: String {
+       return  title ?? "Unknown Title"
+    }
+
+}
+
+extension Movie : Identifiable {
+
+}
