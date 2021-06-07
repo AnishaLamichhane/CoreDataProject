5ffeb3beaa2c9ed3e6ff7ae83ce81e1fe0d25ed1
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-06-07 12:42:43 +0545

    hashable protocol

--- project 12/HashableProtocol.swift
+++ project 12/HashableProtocol.swift
@@ -1,26 +1,27 @@
 //
-//  ContentView.swift
+//  HashableProtocol.swift
 //  project 12
 //
-//  Created by Anisha Lamichhane on 6/5/21.
+//  Created by Anisha Lamichhane on 6/7/21.
 //
 
 import SwiftUI
 
+struct Student: Hashable {
+    let name : String
+}
 
-struct ContentView: View {
+struct HashableProtocol: View {
     let students = [Student(name: "Harry Potter"), Student(name: "Ginger Lashun")]
     var body: some View {
         List(students, id: \.self) { student in
             Text(student.name)
-            
         }
-       
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HashableProtocol_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HashableProtocol()
     }
 }
